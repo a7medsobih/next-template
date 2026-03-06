71b9247a9e623c603cf396e3631d509283d96fe4
Ahmed Sobih <163659912+a7medsobih@users.noreply.github.com>
2026-03-06 03:30:48 +0200

    first commit

---
+++ app/[locale]/page.js
@@ -0,0 +1,11 @@
+// app/[locale]/page.js
+import { useTranslations } from "next-intl";
+
+export default function Home() {
+  const t = useTranslations("HomePage");
+  return (
+    <>
+      <h1>{t("title")}</h1>
+    </>
+  );
+}
